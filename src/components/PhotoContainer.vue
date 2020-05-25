4f735db2be5be4b1b757a6064a1884ef67b596ef
magdalenafernlund <magfer-3@student.ltu.se>
2020-05-25 10:34:44 +0200

    Brutit ut popup till en egen komponent. Lagt till filen PhotoPopUpInfo.vue

--- src/components/PhotoContainer.vue
+++ src/components/PhotoContainer.vue
@@ -25,42 +25,17 @@
         >
       </b-card>
     </b-col>
-    <div
-      class="modal fade"
-      id="exampleModalCenter"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalCenterTitle"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <b-tabs content-class="mt-3">
-            <b-tab title="Beskrivning" active><p>Beskrivning</p></b-tab>
-            <b-tab title="Specifikation"><p>Specifikation</p></b-tab>
-            <b-tab title="Nyckelord"><p>Nyckelord</p></b-tab>
-            <b-tab title="Publikationer"><p>Publikationer</p></b-tab>
-            <b-tab title="Versioner"><p>Versioner</p></b-tab>
-          </b-tabs>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-outline-info waves-effect ml-auto"
-              data-dismiss="modal"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <PopUpInfo></PopUpInfo>
   </div>
 </template>
 
 <script>
+import PopUpInfo from "@/components/PhotoPopUpInfo.vue";
+
 export default {
   name: "PhotoContainer",
   props: ["name", "imgsrc"],
+  components: { PopUpInfo },
   computed: {
     modalId() {
       return `modal-${this.id}`;
---
+++ src/components/PhotoPopUpInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div
+      class="modal fade"
+      id="exampleModalCenter"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalCenterTitle"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <b-tabs content-class="mt-3">
+            <b-tab title="Beskrivning" active><p>Beskrivning</p></b-tab>
+            <b-tab title="Specifikation"><p>Specifikation</p></b-tab>
+            <b-tab title="Nyckelord"><p>Nyckelord</p></b-tab>
+            <b-tab title="Publikationer"><p>Publikationer</p></b-tab>
+            <b-tab title="Versioner"><p>Versioner</p></b-tab>
+          </b-tabs>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-outline-info waves-effect ml-auto"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopUpInfo",
+  computed: {
+    modalId() {
+      return `modal-${this.id}`;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
